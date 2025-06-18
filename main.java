c754c3f839a20ef7653c6a94cecc62e2a8a231e7
Yazeer <mohamedyazeera1211@gmail.com>
2025-06-18 16:33:15 +0530

    If else added

--- main.java
+++ main.java
@@ -1,6 +1,14 @@
 import java.util.*;
 class Main{
     public static void main(String[] args) {
-    System.out.println("hello");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Are u hungry? 1 for Yes, 2 for No");
+        int hungry=sc.nextInt();
+
+        if(hungry==1){
+            System.out.println("Eat smtg");
+        }else{
+            System.out.println("Dont eat");
+        }
 }
 }
